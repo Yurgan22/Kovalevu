f668ba6230a5f72400e4eeca741b53daefa4ab59
Yurgan22 <71118822+Yurgan22@users.noreply.github.com>
2020-11-05 11:49:05 +0300

    Update lab1.cpp

--- Labs/lab1/lab1.cpp
+++ Labs/lab1/lab1.cpp
@@ -1,10 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <math.h>
 using namespace std;
-const double x = -4.5, y = 0.75e-4, z = -84.5;
 int main()
 {
-    double chislitel, znamenatel, vichitaemoe;
+    double chislitel, znamenatel, vichitaemoe, x, y, z;
+    cout << "Vvedite x, y, z: " << endl;
+    cin >> x >> y >> z;
     chislitel = pow(9 + pow(x - y, 2), 1 / 3.);
     znamenatel = x * x + y * y + 2;
     vichitaemoe = exp(fabs(x - y)) * pow(tan(z), 3);
