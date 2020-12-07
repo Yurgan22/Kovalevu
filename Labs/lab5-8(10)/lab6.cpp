37f25fc17e3e05514be1832f9d9bb04e6358c677
Yurgan22 <71118822+Yurgan22@users.noreply.github.com>
2020-12-07 17:51:45 +0300

    Update lab6.cpp

--- Labs/lab5-8(10)/lab6.cpp
+++ Labs/lab5-8(10)/lab6.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <clocale>
 #include <math.h>
 #include <string.h>
@@ -8,23 +8,31 @@ int main()
 	setlocale(LC_ALL, "RUS");
 
 	char str[100];
+	int length_and_index[100][2];
 	cout << "Введите строку : " << endl;
 	gets_s<100>(str);
 
 	bool check = 0;
-	int n = strlen(str) - 1, i = 0, index = 0, len = strlen(str), len_gr;
-
+	int n = strlen(str) - 1, i = 0, len = strlen(str), len_gr;
+	int m = 0;
 	while (str[i] != '\0')
 	{
 		len_gr = 0;
 		while (str[i] == str[i + 1] && i < n) { len_gr++; i++; }
 		len_gr++;
-		if (len_gr < len) { len = len_gr; index = i; }
+		length_and_index[m][0] = len_gr;
+		length_and_index[m][1] = i;
+		if (len_gr < len) len = len_gr;
+		m++;
 		i++;
 	}
-	cout << "Наименьшая по длине группа : " << endl;
-	for (int i = index - len + 1; i <= index; i++)
-		cout << str[i];
-
+	cout << "Наименьшие по длине группы : " << endl;
+	for (int j = 0; j < m; j++)
+		if (length_and_index[j][0] == len)
+		{
+			for (int i = length_and_index[j][1] - len + 1; i <= length_and_index[j][1]; i++)
+				cout << str[i];
+			cout << endl;
+		}
 	return 0;
 }
