49761dbb41f9382f5f37a08680533b1854a543a7
Yurgan22 <71118822+Yurgan22@users.noreply.github.com>
2020-12-07 18:04:10 +0300

    Update lab8.cpp

--- Labs/lab5-8(10)/lab8.cpp
+++ Labs/lab5-8(10)/lab8.cpp
@@ -1,23 +1,28 @@
-﻿#include <iostream> 
+#include <iostream> 
 #include <math.h> 
 #include <iomanip>
 using namespace std;
 typedef double (*uf)(double, double, int&);
 
 void tabl(double, double, double, double, uf);
+void tabl(double, double, double, double, uf, int*);
 double y(double, double, int&);
 double s(double, double, int&);
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 	double a = 0.1;
 	double b = 0.9;
 	double ee = 1e-5;
+	cout << "Введите параметр функции, который хотите увеличить на еденицу: " << endl;
+	int parametr;
+	cin >> parametr;
 	cout << setw(8) << "x" << setw(15) << "y(x)" << setw(10) << "k" << endl;
 	tabl(a, b, 0.1, ee, y);
 	cout << endl;
 	cout << setw(8) << "x" << setw(15) << "s(x)" << setw(10) << "k" << endl;
-	tabl(a, b, 0.1, ee, s);
+	tabl(a, b, 0.1, ee, s, &parametr);
 	return 0;
 }
 
@@ -30,6 +35,19 @@ void tabl(double a, double b, double h, double eps, uf fun)
 		sum = fun(x, eps, k);
 		cout << setw(8) << x << setw(15) << sum << setw(10) << k << endl;
 	}
+}
+
+void tabl(double a, double b, double h, double eps, uf fun, int* parametr)
+{
+	int k = 0;
+	double sum;
+	for (double x = a; x <= b; x += h)
+	{
+		sum = fun(x, eps, k);
+		cout << setw(8) << x << setw(15) << sum << setw(10) << k << endl;
+	}
+	(*parametr)++;
+	cout << "Parametr + 1 = " << *parametr;
 }
 
 double y(double x, double eps, int& k)
